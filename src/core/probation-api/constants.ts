09f96e1f63e293175a799bec50ffc176f175ace7
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-01 13:35:54 +0200

    create a constant to default value of pagination take

--- src/core/probation-api/constants.ts
+++ src/core/probation-api/constants.ts
@@ -1,2 +1,5 @@
 export const PROBATION_API_RETRY_MAX_COUNT = 5;
 export const PROBATION_API_RETRY_DELAY_MS = 1000;
+
+// according to the documentation, range of take - 1 - 1000
+export const PROBATION_API_DEFAULT_PAGINATION_TAKE = 500
