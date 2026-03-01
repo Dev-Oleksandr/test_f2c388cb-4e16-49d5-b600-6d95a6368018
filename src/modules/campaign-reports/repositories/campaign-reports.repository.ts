04888f4d9c0be5f3703c772f8410422dc162b5c6
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-01 17:42:35 +0200

    rewrite request to the DB, devide two requests and compare them in new method

--- src/modules/campaign-reports/repositories/campaign-reports.repository.ts
+++ src/modules/campaign-reports/repositories/campaign-reports.repository.ts
@@ -1,13 +1,15 @@
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { CampaignReport } from '../entities/campaign-report.entity.js';
-import { DeepPartial, Repository } from 'typeorm';
+import { DataSource, DeepPartial, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { PaginationQuery } from '../../../common/decorators/pagination.decorator.js';
 
 @Injectable()
 export class CampaignReportsRepository {
   constructor(
     @InjectRepository(CampaignReport)
     private readonly campaignReportRepository: Repository<CampaignReport>,
+    @InjectDataSource() private readonly dataSource: DataSource,
   ) {}
 
   upsert(data: Array<DeepPartial<CampaignReport>>) {
@@ -17,22 +19,65 @@ export class CampaignReportsRepository {
     });
   }
 
-  async findAggregatedByCondition(condition: {
+  async findAndCountAggregatedByCondition(
+    condition: {
+      fromDate: Date;
+      toDate: Date;
+      eventName: string;
+    },
+    paginationQuery: PaginationQuery,
+  ) {
+    const [rows, { total }] = await Promise.all([
+      this.findAllAggregatedByCondition(condition, paginationQuery),
+      this.countAggregatedByCondition(condition),
+    ]);
+    return { rows, total };
+  }
+
+  private countAggregatedByCondition(condition: {
     fromDate: Date;
     toDate: Date;
     eventName: string;
-    take: number;
   }) {
+    return this.dataSource
+      .createQueryBuilder()
+      .select('COUNT(*)::int', 'total')
+      .from((qb) => {
+        return qb
+          .select('1')
+          .from(CampaignReport, 'cr')
+          .where('cr.event_time BETWEEN :fromDate AND :toDate', {
+            fromDate: condition.fromDate,
+            toDate: condition.toDate,
+          })
+          .andWhere('cr.event_name = :eventName', {
+            eventName: condition.eventName,
+          })
+          .groupBy('cr.ad_id');
+      }, 'grouped')
+      .getRawOne();
+  }
+
+  private findAllAggregatedByCondition(
+    condition: {
+      fromDate: Date;
+      toDate: Date;
+      eventName: string;
+    },
+    { take, skip }: PaginationQuery,
+  ) {
     return this.campaignReportRepository
       .createQueryBuilder()
-      .select('ad_id, COUNT(*) AS count')
+      .select('ad_id as "adId", COUNT(*) AS count')
       .where('event_time BETWEEN :fromDate AND :toDate', {
         fromDate: condition.fromDate,
         toDate: condition.toDate,
       })
       .andWhere('event_name = :eventName', { eventName: condition.eventName })
+      .orderBy('ad_id')
       .groupBy('ad_id')
-      .limit(condition.take)
-      .execute();
+      .take(take)
+      .skip(skip)
+      .getRawMany();
   }
 }
