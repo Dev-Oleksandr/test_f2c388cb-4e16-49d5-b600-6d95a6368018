f3536dfb3d813bcbc050dc3e7d6add92338c1e92
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-01 13:45:11 +0200

    add service, repository and controller in campaign module

--- src/modules/campaign-reports/campaign-reports.module.ts
+++ src/modules/campaign-reports/campaign-reports.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProbationApiModule } from '../../core/probation-api/probation-api.module.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampaignReport } from './entities/campaign-report.entity.js';
+import { CampaignReportsController } from './campaign-reports.controller.js';
+import { CampaignReportsService } from './campaign-reports.service.js';
+import { CampaignReportsRepository } from './repositories/campaign-reports.repository.js';
 
 @Module({
   imports: [ProbationApiModule, TypeOrmModule.forFeature([CampaignReport])],
-  controllers: [],
+  controllers: [CampaignReportsController],
+  providers: [CampaignReportsService, CampaignReportsRepository],
 })
 export class CampaignReportsModule {}
