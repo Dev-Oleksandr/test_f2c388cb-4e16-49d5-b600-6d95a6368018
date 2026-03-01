424242374b6f52226a48b311ca2c7b1e09865571
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-01 18:49:58 +0200

    update linter, fix errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,78 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import stylisticEslintPlugin from '@stylistic/eslint-plugin';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default tseslint.config(
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(['**/eslint.config.mjs']),
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
+    extends: compat.extends(
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ),
+
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      '@stylistic': stylisticEslintPlugin,
+    },
+
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
-      sourceType: 'commonjs',
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json',
       },
     },
-  },
-  {
+
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      '@typescript-eslint/restrict-template-expressions': 'warn'
-      '@typescript-eslint/ no-unsafe-member-access': 'warn'
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@stylistic/lines-between-class-members': [
+        'error',
+        {
+          enforce: [
+            {
+              blankLine: 'always',
+              prev: '*',
+              next: 'method',
+            },
+            {
+              blankLine: 'always',
+              prev: 'method',
+              next: '*',
+            },
+            {
+              blankLine: 'always',
+              prev: 'field',
+              next: 'field',
+            },
+          ],
+        },
+        {
+          exceptAfterSingleLine: true,
+        },
+      ],
     },
   },
-);
+]);
