fe367ea3f215b5c7d2968c5a554e588fd219a7de
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-02 17:08:29 +0200

    revert the connection to DB, make it with DI

--- src/core/database/data-source.ts
+++ src/core/database/data-source.ts
@@ -1,10 +1,15 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
-import { getDatabaseConfig } from './database-config.js';
 dotenv.config();
 
 export default new DataSource({
-  ...getDatabaseConfig(),
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  logging: process.env.DATABASE_LOGGING === 'true',
   migrations: [join(process.cwd(), 'migrations', '*{.ts,.js}')],
 });
--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { getDatabaseConfig } from './database-config.js';
+import { AppConfigService } from '../app-config/app-config.service.js';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (): TypeOrmModuleOptions => ({
-        ...getDatabaseConfig(),
+      inject: [AppConfigService],
+      useFactory: (configService: AppConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        logging: configService.get('DATABASE_LOGGING'),
         autoLoadEntities: true,
       }),
     }),
