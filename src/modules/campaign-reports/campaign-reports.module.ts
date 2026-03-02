37211beea900ee73add2d1a8376e468678699b37
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-02 17:06:15 +0200

    add new entity and repository to CampaignReportsModule

--- src/modules/campaign-reports/campaign-reports.module.ts
+++ src/modules/campaign-reports/campaign-reports.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { ProbationApiModule } from '../../core/probation-api/probation-api.module.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampaignReport } from './entities/campaign-report.entity.js';
+import { CampaignReportJob } from './entities/campaign-report-job.entity.js';
 import { CampaignReportsController } from './campaign-reports.controller.js';
 import { CampaignReportsService } from './campaign-reports.service.js';
 import { CampaignReportsRepository } from './repositories/campaign-reports.repository.js';
+import { CampaignReportJobRepository } from './repositories/campaign-report-jobs.repository.js';
 
 @Module({
-  imports: [ProbationApiModule, TypeOrmModule.forFeature([CampaignReport])],
+  imports: [
+    ProbationApiModule,
+    TypeOrmModule.forFeature([CampaignReport, CampaignReportJob]),
+  ],
   controllers: [CampaignReportsController],
-  providers: [CampaignReportsService, CampaignReportsRepository],
+  providers: [
+    CampaignReportsService,
+    CampaignReportsRepository,
+    CampaignReportJobRepository,
+  ],
 })
 export class CampaignReportsModule {}
