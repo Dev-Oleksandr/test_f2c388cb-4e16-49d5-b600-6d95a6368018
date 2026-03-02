85afcd1ed6d584f94d9ee98a95764631e0bdc5b4
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-02 17:08:02 +0200

    add new private methods, mark jobs with status (pending, processing, completed, failed), refactoring

--- src/modules/campaign-reports/campaign-reports.service.ts
+++ src/modules/campaign-reports/campaign-reports.service.ts
@@ -1,17 +1,18 @@
 import { ProbationApiService } from '../../core/probation-api/probation-api.service.js';
-import { Injectable, Logger } from '@nestjs/common';
-import { SyncCampaignReportsQueryDto } from './schemas/sync-campaign-reports-query.schema.js';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { SyncCampaignReportsDto } from './schemas/sync-campaign-reports-query.schema.js';
 import {
   CampaignReportParsedCsvRow,
   CampaignReportsRequest,
 } from '../../core/probation-api/types.js';
 import { CampaignReportsRepository } from './repositories/campaign-reports.repository.js';
-import { concatMap, lastValueFrom } from 'rxjs';
+import { concatMap, lastValueFrom, merge, Observable, share, take } from 'rxjs';
 import { PROBATION_API_DEFAULT_PAGINATION_TAKE } from '../../core/probation-api/constants.js';
 import { GetAggregatedCampaignReportsDto } from './schemas/get-aggregated-campaign-reports.schema.js';
 import { DataListResponse } from '../../common/dto/data-list-response.dto.js';
 import { PaginationQuery } from '../../common/decorators/pagination.decorator.js';
-import { SuccessResponse } from '../../common/success-response.js';
+import { CampaignReportJobRepository } from './repositories/campaign-report-jobs.repository.js';
+import { AppException } from '../../common/exceptions/app.exception.js';
 
 @Injectable()
 export class CampaignReportsService {
@@ -20,27 +21,23 @@ export class CampaignReportsService {
   constructor(
     private readonly probationApiService: ProbationApiService,
     private readonly campaignReportsRepository: CampaignReportsRepository,
+    private readonly campaignReportJobRepository: CampaignReportJobRepository,
   ) {}
 
-  syncCampaignReports(dto: SyncCampaignReportsQueryDto) {
+  async syncCampaignReports(dto: SyncCampaignReportsDto) {
     const request = this.formatDtoToRequest(dto);
-    const observableCsvRows =
-      this.probationApiService.fetchAllCampaignReports(request);
 
-    lastValueFrom(
-      observableCsvRows.pipe(
-        concatMap((rows) =>
-          this.campaignReportsRepository.upsert(
-            this.mapFetchedFieldToUpsert(rows),
-          ),
-        ),
-      ),
-    ).catch((error) => {
-      const message = 'An error occurred while fetching campaign reports';
-      this.logger.error(message, error.message);
+    const job = await this.campaignReportJobRepository.create({
+      status: 'pending',
     });
 
-    return SuccessResponse;
+    const fetchedCsvRows = this.probationApiService
+      .fetchAllCampaignReports(request)
+      .pipe(share());
+
+    this.executeSyncJob(job.id, fetchedCsvRows);
+
+    return job;
   }
 
   async findAggregatedCampaignReports(
@@ -56,8 +53,74 @@ export class CampaignReportsService {
     return new DataListResponse(rows, pagination, total);
   }
 
+  async getCampaignReportSyncJob(jobId: string) {
+    const job = await this.campaignReportJobRepository.getByCondition({
+      id: jobId,
+    });
+
+    if (!job) {
+      throw new AppException({
+        message: 'Job not found in the system',
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+
+    return job;
+  }
+
+  private executeSyncJob(
+    jobId: string,
+    csvRows: Observable<CampaignReportParsedCsvRow[]>,
+  ): void {
+    const setProcessing = this.createProcessingStatusStream(jobId, csvRows);
+    const processData = this.createDataProcessingStream(csvRows);
+    lastValueFrom(merge(setProcessing, processData))
+      .then(() => this.completeJob(jobId))
+      .catch((error) => this.failJob(jobId, error));
+  }
+
+  private createProcessingStatusStream(
+    jobId: string,
+    csvRows: Observable<CampaignReportParsedCsvRow[]>,
+  ) {
+    return csvRows.pipe(
+      take(1),
+      concatMap(() =>
+        this.campaignReportJobRepository.update(jobId, {
+          status: 'processing',
+        }),
+      ),
+    );
+  }
+
+  private createDataProcessingStream(
+    csvRows: Observable<CampaignReportParsedCsvRow[]>,
+  ) {
+    return csvRows.pipe(
+      concatMap((rows) =>
+        this.campaignReportsRepository.upsert(
+          this.mapFetchedFieldToUpsert(rows),
+        ),
+      ),
+    );
+  }
+
+  private completeJob(jobId: string): Promise<any> {
+    return this.campaignReportJobRepository.update(jobId, {
+      status: 'completed',
+    });
+  }
+
+  private failJob(jobId: string, error: Error): Promise<any> {
+    this.logger.error(`Error in sync job ${jobId}`, error.stack);
+    return this.campaignReportJobRepository.update(jobId, {
+      status: 'failed',
+      errorMessage: error.message,
+    });
+  }
+
   private formatDtoToRequest(
-    dto: SyncCampaignReportsQueryDto,
+    dto: SyncCampaignReportsDto,
   ): CampaignReportsRequest {
     return {
       to_date: dto.toDate.toISOString(),
