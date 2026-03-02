98ff4e2638965a24e92eef740083017e2e956c4b
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-02 10:36:51 +0200

    add timeout config to axios http module in probation api module

--- src/core/probation-api/probation-api.module.ts
+++ src/core/probation-api/probation-api.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AppConfigService } from '../app-config/app-config.service.js';
 import { ProbationApiService } from './probation-api.service.js';
+import { PROBATION_API_TIMEOUT } from './constants.js';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ProbationApiService } from './probation-api.service.js';
       useFactory: (configService: AppConfigService) => ({
         baseURL: configService.get('PROBATION_API_BASE_URL'),
         headers: { 'x-api-key': configService.get('PROBATION_API_KEY') },
+        timeout: PROBATION_API_TIMEOUT,
       }),
     }),
   ],
