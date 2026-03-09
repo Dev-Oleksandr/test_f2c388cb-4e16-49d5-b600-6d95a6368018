00e633f446718b6fcecf682da472bb92e8d44a99
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-09 09:23:30 +0200

    refactoring

--- src/core/probation-api/probation-api.service.ts
+++ src/core/probation-api/probation-api.service.ts
@@ -10,7 +10,7 @@ import {
   CampaignReportsRequest,
   CampaignReportsResponse,
 } from './types.js';
-import { CampaignReportEventName } from '../../common/types/campaign-report-event-name.type.js';
+import { CampaignReportCsvParser } from './campaign-report-csv-parser.js';
 
 @Injectable()
 export class ProbationApiService {
@@ -32,7 +32,7 @@ export class ProbationApiService {
 
         return this.fetchApi(nextUrl);
       }),
-      map(({ data }) => this.parseCsv(data.csv)),
+      map(({ data }) => CampaignReportCsvParser.parse(data.csv)),
     );
   }
 
@@ -48,60 +48,6 @@ export class ProbationApiService {
       }),
       map(({ data }) => data),
     );
-  }
-
-  private parseCsv(csv: string): Array<CampaignReportParsedCsvRow> {
-    const lines = csv.split('\n').filter((line) => line.trim().length > 0);
-
-    if (lines.length < 2) {
-      return [];
-    }
-
-    const headerLine = lines[0];
-
-    const headers = headerLine
-      ? headerLine.split(',').map((header) => header.trim())
-      : undefined;
-
-    const requiredHeaders = this.ensureRequiredHeaders(headers);
-
-    return lines.slice(1).map((line) => {
-      const values = line.split(',').map((v) => v.trim());
-      const rawRow = Object.fromEntries(
-        requiredHeaders.map((header, index) => [header, values[index] ?? '']),
-      ) as Record<keyof CampaignReportParsedCsvRow, string>;
-
-      return {
-        ...rawRow,
-        event_time: new Date(rawRow.event_time),
-        event_name: rawRow.event_name as CampaignReportEventName,
-      };
-    });
-  }
-
-  private ensureRequiredHeaders(headers?: Array<string>) {
-    const requiredHeaders = [
-      'ad',
-      'ad_id',
-      'adgroup',
-      'adgroup_id',
-      'campaign',
-      'campaign_id',
-      'client_id',
-      'event_name',
-      'event_time',
-    ];
-    const missingHeaders = requiredHeaders.filter(
-      (requiredHeader) => !headers?.includes(requiredHeader),
-    );
-
-    if (missingHeaders.length) {
-      throw new Error(
-        `Invalid CSV: missing headers: ${missingHeaders.join(', ')}`,
-      );
-    }
-
-    return requiredHeaders;
   }
 
   private buildQueryString(dto: CampaignReportsRequest): string {
