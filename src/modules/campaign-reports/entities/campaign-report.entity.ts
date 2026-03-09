1a740f2f069e9733771b41a55d21322ec719255e
Oleksandr Dovbnia <softw.oleksandr@gmail.com>
2026-03-09 09:02:38 +0200

    refactor in campaign report entity

--- src/modules/campaign-reports/entities/campaign-report.entity.ts
+++ src/modules/campaign-reports/entities/campaign-report.entity.ts
@@ -1,7 +1,12 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
 import { CampaignReportEventName } from '../../../common/types/campaign-report-event-name.type.js';
 
 @Entity('campaign_reports')
+@Index('IDX_campaign_reports_event_time_event_name_ad_id', [
+  'eventTime',
+  'eventName',
+  'adId',
+])
 export class CampaignReport {
   @Column()
   campaign!: string;
@@ -21,12 +26,12 @@ export class CampaignReport {
   @Column({ name: 'ad_id', type: 'uuid' })
   adId!: string;
 
-  @Column({ name: 'client_id', type: 'uuid', primary: true })
+  @PrimaryColumn({ name: 'client_id', type: 'uuid' })
   clientId!: string;
 
-  @Column({ name: 'event_name', primary: true })
+  @PrimaryColumn({ name: 'event_name' })
   eventName!: CampaignReportEventName;
 
-  @Column({ name: 'event_time', type: 'timestamp', primary: true })
+  @PrimaryColumn({ name: 'event_time', type: 'timestamp' })
   eventTime!: Date;
 }
